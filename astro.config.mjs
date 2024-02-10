b06d58c68fed887e4993093d3288ac0298721ff9
cpluu <cuong.luu4o8@gmail.com>
2024-02-10 17:43:45 -0600

    GitHub Pages set-up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,39 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
+// import { defineConfig } from "astro/config";
+// import sitemap from "@astrojs/sitemap";
+// import netlify from "@astrojs/netlify";
+// import robotsTxt from "astro-robots-txt";
+// import UnoCSS from "@unocss/astro";
+// import icon from "astro-icon";
 
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
+// import solidJs from "@astrojs/solid-js";
+// import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
-// https://astro.build/config
+// // https://astro.build/config
+// export default defineConfig({
+//   site: "https://gianmarco.xyz/",
+//   integrations: [
+//     sitemap(),
+//     robotsTxt({
+//       sitemap: [
+//         "https://gianmarco.xyz/sitemap-index.xml",
+//         "https://gianmarco.xyz/sitemap-0.xml",
+//       ],
+//     }),
+//     solidJs(),
+//     UnoCSS({ injectReset: true }),
+//     icon()
+//   ],
+//   markdown: {
+//     remarkPlugins: [remarkReadingTime],
+//   },
+//   output: "server",
+//   adapter: netlify(),
+// });
+
+
+import { defineConfig } from 'astro/config'
+
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon()
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  output: "server",
-  adapter: netlify(),
-});
+  site: 'https://cpluu.github.io/',
+  // base: '/my-repo',
+})
