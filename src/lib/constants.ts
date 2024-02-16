45a42ef667ed46f6a83f71ee38865255c7d8319c
cpluu <cuong.luu4o8@gmail.com>
2024-02-16 13:58:58 -0600

    Tidying loose ends (v1.3)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,7 @@ export const LINKS = {
   linkedin: "https://www.linkedin.com/in/cuong-luu/",
   medium: "https://medium.com/@cpluu",
   cv: "https://docs.google.com/document/d/1Uy9PPBPVC4ohgdxADSii_V87lpt3yuUmBIt-AYjYbMI/edit?usp=sharing",
+  googlescholar: "https://scholar.google.com/citations?user=tWeLeEUAAAAJ&hl=en&oi=sra",
   instagram: "https://www.instagram.com/postcall_luucp/",
   bluesky: "https://bsky.app/profile/cluu.bsky.social",
 };
